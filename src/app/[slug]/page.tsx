79d5693611eca2c30036a88bb11499c17b128516
Alan Mooiman <amooiman@gmail.com>
2025-07-07 00:19:32 -0400

    WTf

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,8 +2,9 @@ export async function generateStaticParams() {
   return [];
 }
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
