0662a0f9368baf0dcd000267cebe427802edaa69
Alan Mooiman <amooiman@gmail.com>
2025-07-07 00:00:31 -0400

    Update page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -7,7 +7,7 @@ export default function Page({ params }: { params: { slug: string } }) {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Idk here's a page with the slug of <strong>{slug}</strong>
+      Idk here is a page with the slug of <strong>{slug}</strong>
     </div>
   );
 }
