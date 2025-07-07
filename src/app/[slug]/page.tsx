3e74f4564ef352203c93abf507686098b1d9c0e7
Alan Mooiman <amooiman@gmail.com>
2025-07-07 00:14:08 -0400

    Ok fine make it sync, let's see

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,8 +2,8 @@ export async function generateStaticParams() {
   return [];
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+export default function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
